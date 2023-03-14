d00c1e6307f9dfcb82c06c547b3aaf5a5707a441
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-03-14 12:47:05 +0800

    feat: 修改名称

--- bin/main.js
+++ bin/main.js
@@ -5,14 +5,14 @@ const { chalk } = require("@vue/cli-shared-utils")
 const create = require("../lib/create")
 
 program
-  .version(`next-cli ${require("../package").version}`)
+  .version(`ghx-cli ${require("../package").version}`)
   .usage("<command> [options]")
 
 program
   .command("create <app-name>")
   .description("创建项目")
   .action((name, options) => {
-    console.log(chalk.bold.blue(`Next CLI V1.0.0`))
+    console.log(chalk.bold.blue(`Ghx CLI V1.0.0`))
     create(name, options)
   })
 
@@ -20,7 +20,7 @@ program.on("--help", () => {
   console.log()
   console.log(
     `  Run ${chalk.yellow(
-      `next-cli <command> --help`
+      `ghx-cli <command> --help`
     )} for detailed usage of given command.`
   )
   console.log()
